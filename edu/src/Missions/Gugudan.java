8fea258c90cf6b5061c0da1b888d608a5d749443
SuHyunParkSunshine <suhyun5701@gmail.com>
2023-04-13 20:05:15 +0900

    23.04.13_Study

---
+++ edu/src/Missions/Gugudan.java
@@ -0,0 +1,15 @@
+package Missions;
+
+public class Gugudan {
+
+	public static void main(String[] args) {
+
+		for (int k = 1; k < 10; k++) {
+
+			for (int i = 1; i < 10; i++) {
+				System.out.println(k + "x" + i + " = " + (i * k));
+			}
+		}
+
+	}
+}
--- edu/src/Missions/Test.java
+++
@@ -1,8 +0,0 @@
-package Missions;
-
-public class Test {
-	public static void main(String[] args) {
-		int a = 1.2;
-	}
-
-}
