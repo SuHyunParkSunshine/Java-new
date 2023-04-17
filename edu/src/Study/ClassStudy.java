7e07f4d58e990b55a385f5a47e1ca77def54b5e9
SuHyunParkSunshine <suhyun5701@gmail.com>
2023-04-17 20:06:03 +0900

    23.04.17_Study(2)

--- edu/src/Study/ClassStudy.java
+++ edu/src/Study/ClassStudy.java
@@ -4,19 +4,26 @@ class FishFrame {
 	
 	int fid; // = this.fid 클래스 안에 있는 변수들을 필드 혹은 맴버변수, 속성이라고 부름
 	String color;
+	int eyes = 2;
 	
 	FishFrame() { //case1 => 생성자
 		this.fid = 0;
 		this.color = "노란색";
 	}
 	
-	FishFrame(int fid, String color) { //case2 => 생성자
+	FishFrame(int fid, String color) { //case2 => 생성자/ 같은 이름의 생성자가 2개인 경우 -> 오버로딩이라구 한다.
 		this.fid = fid;
 		this.color = color;
 	}
-//	int cut () { // 함수, 매서드, 멤버함수-> 클래스 안에 들어가 있는 함수
-//		
-//	}
+	int getEyes () { // 함수, 매서드, 멤버함수-> 클래스 안에 들어가 있는 함수
+		return this.eyes;
+	}
+	void setFish(FishFrame fish) {
+		this.fid = fish.fid;
+		this.color = fish.color;
+		
+		return; //void는 리턴 값 없어서 그냥 안 적어도 댐
+	}
 }
 
 public class ClassStudy {
@@ -31,6 +38,13 @@ public class ClassStudy {
 		
 		System.out.println(fish2.fid);
 		System.out.println(fish2.color); //새로 넣은 값을 출력해줌
+		
+		int e = fish2.getEyes();
 	}
+	FishFrame getFish() {
+		FishFrame fish = new FishFrame();
+		
+		return fish;
+	}	
 
 }
