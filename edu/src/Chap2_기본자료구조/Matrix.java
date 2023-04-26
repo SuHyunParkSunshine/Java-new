fae7897070ba3cab5784ea537cea71fa9e0a3818
SuHyunParkSunshine <suhyun5701@gmail.com>
2023-04-26 14:19:01 +0900

    23.04.26_자습

--- edu/src/Chap2_기본자료구조/Matrix.java
+++ edu/src/Chap2_기본자료구조/Matrix.java
@@ -1,5 +1,6 @@
 package Chap2_기본자료구조;
 
+import java.util.Arrays;
 import java.util.Random;
 
 public class Matrix {
@@ -7,7 +8,7 @@ public class Matrix {
 	static void getArr(int[][] items) {
 		Random rand = new Random();
 		for (int i = 0; i < items.length; i++) {
-			for (int j = 0; j < items.length + 1; j++) {
+			for (int j = 0; j < items[i].length ; j++) {
 				items[i][j] = rand.nextInt(10);
 			}
 		}
@@ -15,7 +16,7 @@ public class Matrix {
 
 	static void showArr(int[][] items) {
 		for (int i = 0; i < items.length; i++) {
-			for (int j = 0; j < items.length + 1; j++) {
+			for (int j = 0; j < items[i].length ; j++) {
 				System.out.print(items[i][j] + "\t");
 			}
 			System.out.print("\n");
@@ -34,12 +35,23 @@ public class Matrix {
 
 	public static void main(String[] args) {
 		int [][] A = new int[2][3];
+		int [][] A1 = new int[2][3];
 		int [][] B = new int[3][4];
 		int [][] C = new int[2][4];
 		int [][] D = new int[3][2];
 		
+		
 		getArr(A);
 		showArr(A);
+		System.out.println("=".repeat(50));
+		getArr(A1);
+		showArr(A1);
+		System.out.println("=".repeat(50));
+		getArr(B);
+		showArr(B);
+		System.out.println("=".repeat(50));
+		int [][] A2 = addMatrix(A, A1);
+		showArr(A2);
 	}
 
 }
