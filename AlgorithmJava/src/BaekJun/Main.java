8c08259141eb6bdd45c1a6604a7e66d01ea9af4e
SuHyunParkSunshine <suhyun5701@gmail.com>
2023-06-01 13:12:00 +0900

    23.06.01_백준 4949번

--- AlgorithmJava/src/BaekJun/Main.java
+++ AlgorithmJava/src/BaekJun/Main.java
@@ -4,43 +4,54 @@ import java.util.Scanner;
 import java.util.Stack;
 
 public class Main {
-	
+
 	public static void main(String[] args) {
-		
+
 		Scanner sc = new Scanner(System.in);
 
-		int T = sc.nextInt();
-		
-		for(int i = 0; i < T; i++) {
-			String s = sc.next();
-			System.out.println(solve(s));			
+		String s;
+
+		while (true) {
+			s = sc.nextLine();
+
+			if (s.equals(".")) {
+				break;
+			}
+			System.out.println(solve(s));
 		}
 		sc.close();
 	}
-	
+
 	public static String solve(String str) {
 		Stack<Character> stk = new Stack<>();
-		
-		for(int j = 0; j < str.length(); j++) {
-			
+
+		for (int j = 0; j < str.length(); j++) {
+
 			char c = str.charAt(j);
-			
-			if(c == '(') {
+
+			if (c == '(' || c == '[') {
 				stk.push(c);
 			}
-			else if (stk.isEmpty()) {
-				return "NO";
+
+			else if (c == ')') {
+				if (stk.isEmpty() || stk.peek() != '(') {
+					return "no";
+				}
+				stk.pop();
 			}
-			else {
+
+			else if (c == ']') {
+				if (stk.isEmpty() || stk.peek() != '[') {
+					return "no";
+				}
 				stk.pop();
 			}
 		}
-		
-		if(stk.isEmpty()) {
-			return "YES";
-		}
-		else {
-			return "NO";
+
+		if (stk.isEmpty()) {
+			return "yes";
+		} else {
+			return "no";
 		}
 	}
 }
